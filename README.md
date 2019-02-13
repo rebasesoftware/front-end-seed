f710752fbe9678569231eb5b326cf3102c57a6f0
Fikret Turşak <fikret@rebase.com.tr>
2019-02-13 17:20:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Front-end Seed  
+> 🚀 Kickass front-end boilerplate with TypeScript, Sass, webpack and much more.  
+
+## Built upon    
+- [x] [Typescript 3](https://www.typescriptlang.org/)  
+- [x] [Sass](http://sass-lang.com/)
+- [x] [Bootstrap 4](https://getbootstrap.com/)
+- [x] [Webpack 4](https://webpack.js.org/)  
+- [x] [Autoprefixer](https://github.com/postcss/autoprefixer)  
+- [x] [Imagemin](https://github.com/Klathmon/imagemin-webpack-plugin#readme)  
+- [x] [TSLint](https://palantir.github.io/tslint/)  
+  
+## Usage  
+### Development  
+```bash  
+npm run serve  
+```  
+Starts a development server and navigates to ```localhost:3000```.  
+  
+### Build  
+```bash  
+npm run build  
+```  
+Builds the production ready package into ```dist``` folder.
+
+## License
+`Front-end Seed` is released under the MIT license. See LICENSE for details.
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = (env, arg) => ({
+  mode: 'production',
+  entry: './src/app/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          'ts-loader',
+          'tslint-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: arg.mode === 'development' ? 'expanded' : 'compressed'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new CopyWebpackPlugin(
+      [
+        { from: 'src/public', to: '../' }
+      ],
+      { ignore: ['.DS_Store', 'Thumbs.db'] }
+    ),
+    new WriteFilePlugin(),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      jpegtran: {
+        progressive: true
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.scss']
+  },
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist/assets')
+  },
+  devtool: arg.mode === 'development' ? 'source-map' : false,
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000
+  },
+  performance: {
+    hints: false
+  }
+});
